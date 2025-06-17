e3f3ad3700bdf3eb56a30a3c2c858991e50eb5f8
Marvofops <aamasiimweallan@gmail.com>
2025-06-17 15:26:30 +0300

    Update settings.py

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -135,6 +135,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_URL = '/login_user'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
