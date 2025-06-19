3e662863c81f82cf139e62832ab4a9dad57ac115
Marvofops <aamasiimweallan@gmail.com>
2025-06-19 16:55:08 +0300

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     path('delete_task/<int:task_id>/',views.delete_task, name='delete_task'),
     path('register_user/', views.register_user, name='register_user'),
     path('login_user/', views.login_user, name='login_user'),
-    
+    path('login/',views.login_user, name='login'),
     path('logout_user/', views.logout_user, name='logout_user'),
     path('update/<int:task_id>', views.update, name='update'),
 ]
