2cb56c5b8aa6e3828ddc60c97bfdaa0881440b2d
Marvofops <aamasiimweallan@gmail.com>
2025-06-19 17:04:01 +0300

    Update settings.py

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*j&pl_=nn92ckq&n$*rd$7l*ues*(65vn^2oydufr#rgdx-&1p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
