6c474f42d0f9d393ed982b52095e94e13ed60570
Marvofops <aamasiimweallan@gmail.com>
2025-06-19 16:53:32 +0300

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -73,8 +73,8 @@ def register_user(request):
    
 def login_user(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user.set_password(password)
         user = authenticate(request, username=username, password=password)
         if user is not None:
